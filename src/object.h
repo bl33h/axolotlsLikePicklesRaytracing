afdf8780cc32285f860e4225ded76e806a3f9505
bl33h <per21371@uvg.edu.gt>
2023-11-22 00:33:36 -0600

    documentation made

--- src/object.h
+++ src/object.h
@@ -11,17 +11,23 @@ Last modification: 21/11/2023
 #include "materials.h"
 #include "glm/glm.hpp"
 
+// struct representing information about the intersection of a ray with an object
 struct Intersect {
-    bool isIntersecting = false;
-    float dist = 0.0f;
-    glm::vec3 point;
-    glm::vec3 normal;
+    bool isIntersecting = false; // indicates whether the ray intersects with the object
+    float dist = 0.0f;            // distance from the ray origin to the intersection point
+    glm::vec3 point;              // intersection point coordinates
+    glm::vec3 normal;             // normal vector at the intersection point
 };
 
+// abstract base class for objects in the scene
 class Object {
 public:
+    // constructor with material initialization
     Object(const Material& mat) : material(mat) {}
+
+    // virtual function for ray intersection, to be implemented by derived classes
     virtual Intersect rayIntersect(const glm::vec3& rayOrigin, const glm::vec3& rayDirection) const = 0;
 
+    // material of the object
     Material material;
 };
