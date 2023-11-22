2372f0545d00580a2bd5b749ad640ad820f3da64
bl33h <per21371@uvg.edu.gt>
2023-11-21 23:11:33 -0600

    materials for creating the axolotl and two pickles placed

---
+++ src/materials.cpp
@@ -0,0 +1,110 @@
+#include "colors.h"
+#include "materials.h"
+
+Material rubber = {
+    Color(155, 155, 155),
+    0.9,
+    0.1,
+    10.0f,
+    0.0f,
+    0.0f
+};
+
+Material grass = {
+    Color(57, 89, 123),
+    0.9,
+    0.1,
+    10.0f,
+    0.0f,
+    0.4f
+};
+
+Material mold = {
+    Color(0, 90, 118),
+    0.9,
+    0.1,
+    10.0f,
+    0.0f,
+    0.0f
+};
+
+Material lights = {
+    Color(224, 166, 79),
+    0.5,
+    0.1,
+    10.0f,
+    0.7f,
+    0.4f
+};
+
+Material carbon = {
+    Color(54, 13, 65),
+    0.9,
+    0.1,
+    10.0f,
+    0.0f,
+    0.0f
+};
+
+Material smile = {
+    Color(112, 60, 86),
+    0.9,
+    0.1,
+    10.0f,
+    0.0f,
+    0.0f
+};
+
+Material barbie = {
+    Color(251, 200, 233),
+    0.9,
+    5,
+    0.0f,
+    0.0f,
+    0.0f,
+};
+
+Material popsicle = {
+    Color(252, 194, 230),
+    0.9,
+    1,
+    0.0f,
+    0.2f,
+    0.0f,
+};
+
+Material straw = {
+    Color(255, 192, 189),
+    0.7,
+    0.1,
+    10.0f,
+    0.0f,
+    0.1f
+};
+
+Material coal = {
+    Color(255, 192, 189),
+    0.5,
+    0.1,
+    10.0f,
+    0.7f,
+    0.4f
+};
+
+Material darkCoal = {
+    Color(255, 192, 189),
+    0.5,
+    0.1,
+    10.0f,
+    0.0f,
+    0.4f
+};
+
+Material white = {
+Color(170, 170, 170),
+0.9,
+0.1,
+10.0f,
+0.0f,
+0.0f
+};
---
+++ src/materials.h
@@ -0,0 +1,45 @@
+/*---------------------------------------------------------------------------
+Copyright (C), 2022-2023, Sara Echeverria (bl33h)
+@author Sara Echeverria
+FileName: materials.h
+@version: I
+Creation: 07/11/2023
+Last modification: 21/11/2023
+*Some parts were made using the AIs Bard and ChatGPT
+------------------------------------------------------------------------------*/
+#pragma once
+#include "glm/glm.hpp"
+#include "colors.h"
+
+struct Material {
+    Color diffuse;
+    float albedo;
+    float specularAlbedo;
+    float specularCoefficient;
+    float reflectivity;
+    float transparency;
+    float refractionIndex;
+};
+
+struct Light {
+    glm::vec3 position;
+    float intensity;
+    Color color;
+
+    Light(const glm::vec3& pos, float intensity, const Color& col)
+        : position(pos), intensity(intensity), color(col) {}
+    
+};
+
+extern Material rubber;
+extern Material grass;
+extern Material mold;
+extern Material lights;
+extern Material carbon;
+extern Material smile;
+extern Material barbie;
+extern Material popsicle;
+extern Material straw;
+extern Material coal;
+extern Material darkCoal;
+extern Material white;
