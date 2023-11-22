79657a0a9615fb614a88c73550052bc13f91805c
bl33h <per21371@uvg.edu.gt>
2023-11-22 00:56:12 -0600

    documentation made

--- src/skybox.h
+++ src/skybox.h
@@ -14,12 +14,19 @@ Last modification: 21/11/2023
 
 class Skybox {
 public:
+    // constructor: Initializes the Skybox with the specified texture file
     Skybox(const std::string& textureFile);
+
+    // destructor: Frees the allocated texture surface
     ~Skybox();
 
+    // retrieves the color of the skybox in the specified direction
     Color getColor(const glm::vec3& direction) const;
 
 private:
+    // surface to store the skybox texture
     SDL_Surface* texture;
+
+    // loads the skybox texture from the specified file
     void loadTexture(const std::string& textureFile);
 };
