ddfa13ed627ae415c68a15d7c6ebb68a6a2a1bc7
bl33h <per21371@uvg.edu.gt>
2023-11-22 00:27:49 -0600

    documentation made

--- src/cube.h
+++ src/cube.h
@@ -14,16 +14,22 @@ Last modification: 21/11/2023
 
 class Cube : public Object {
 public:
+    // constructor to initialize Cube with minimum and maximum corners and material
     Cube(const glm::vec3& minCorner, const glm::vec3& maxCorner, const Material& mat);
 
+    // function to perform ray intersection with the cube
     Intersect rayIntersect(const glm::vec3& rayOrigin, const glm::vec3& rayDirection) const override;
 
+    // function to set the texture of the cube
     void setTexture(SDL_Texture* tex) {
         texture = tex;
     }
 
 private:
+    // minimum and maximum corners of the cube
     glm::vec3 minCorner;
     glm::vec3 maxCorner;
+
+    // texture of the cube
     SDL_Texture* texture;
 };
