b9255add32fccb4d79c58ef52aa77c00f519c028
bl33h <per21371@uvg.edu.gt>
2023-11-22 00:30:39 -0600

    documentation made

--- src/materials.h
+++ src/materials.h
@@ -11,26 +11,29 @@ Last modification: 21/11/2023
 #include "colors.h"
 #include "glm/glm.hpp"
 
+// struct representing material properties
 struct Material {
-    Color diffuse;
-    float albedo;
-    float specularAlbedo;
-    float specularCoefficient;
-    float reflectivity;
-    float transparency;
-    float refractionIndex;
+    Color diffuse;            // Diffuse color of the material
+    float albedo;             // Surface reflectivity
+    float specularAlbedo;     // Specular reflection albedo
+    float specularCoefficient; // Specular reflection coefficient
+    float reflectivity;        // Reflectivity of the material
+    float transparency;        // Transparency of the material
+    float refractionIndex;     // Refraction index of the material
 };
 
+// struct representing a light source
 struct Light {
-    glm::vec3 position;
-    float intensity;
-    Color color;
+    glm::vec3 position; // position of the light source
+    float intensity; // intensity of the light
+    Color color; // color of the light
 
+    // constructor
     Light(const glm::vec3& pos, float intensity, const Color& col)
         : position(pos), intensity(intensity), color(col) {}
-    
 };
 
+// external declarations of material instances
 extern Material rubber;
 extern Material grass;
 extern Material mold;
